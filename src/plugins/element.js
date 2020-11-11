44093e65e68bcecfd5d00c497395e4303bfbd2f6
unknown <1768941378@qq.com>
2020-11-11 19:33:31 +0800

    完成了登錄功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+// 导入弹框提示组件
+// import { Message } from 'element-ui'
 
-Vue.use(Button)
+Vue.use(Button).use(Form).use(FormItem).use(Input)
+Vue.prototype.$message = Message
---
+++ src/router.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  { path: '/home', component: Home }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+// 挂载路由守卫 控制没有登录状态直接跳转到登录页
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从那个路径跳转过来
+  // next 是一个函数，next()表示放行  next('xxx') 表示强制跳转
+  if (to.path === '/login') return next()
+  // 获取浏览器中的token进行认证
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+
+export default router
