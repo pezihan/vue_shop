08d9fe2b07277b91606188ef3f21c4181dc5e67f
unknown <1768941378@qq.com>
2020-11-15 22:06:09 +0800

    完成商品功能

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ import '@/assets/css/global.css'
 import '@/assets/fonts/iconfont.css'
 import TreeTable from 'vue-table-with-tree-grid'
 
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
 import axios from 'axios'
 // 配置请求的后端根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
@@ -23,6 +30,22 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+
+// 将富文本选择器注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
+// 将时间处理函数挂在到vue的上全局可以使用(vue过滤器) 传入毫秒数转换成时间格式
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear() // 年
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0') // 月
+  const d = (dt.getDate() + '').padStart(2, '0') // 日
+
+  const hh = (dt.getHours() + '').padStart(2, '0') // 小时
+  const mm = (dt.getMinutes() + '').padStart(2, '0') // 分
+  const ss = (dt.getSeconds() + '').padStart(2, '0') // 秒
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -31,7 +31,12 @@ import {
   Cascader,
   Alert,
   Tabs,
-  TabPane
+  TabPane,
+  Steps,
+  Step,
+  Checkbox,
+  CheckboxGroup,
+  Upload
 } from 'element-ui'
 // 导入弹框提示组件
 // import { Message } from 'element-ui'
@@ -40,4 +45,5 @@ Vue.prototype.$confirm = MessageBox.confirm
 Vue.prototype.$message = Message // 弹窗组件必须挂在在vue的原型上
 Vue.use(Button).use(Form).use(FormItem).use(Input).use(Container).use(Header).use(Aside).use(Main).use(Menu).use(Submenu).use(MenuItem)
 Vue.use(Breadcrumb).use(BreadcrumbItem).use(Card).use(Row).use(Col).use(Table).use(TableColumn).use(Switch).use(Tooltip).use(Pagination).use(Dialog)
-Vue.use(Tag).use(Tree).use(Select).use(Option).use(Cascader).use(Alert).use(Tabs).use(TabPane)
+Vue.use(Tag).use(Tree).use(Select).use(Option).use(Cascader).use(Alert).use(Tabs).use(TabPane).use(Step).use(Steps)
+Vue.use(Checkbox).use(CheckboxGroup).use(Upload)
